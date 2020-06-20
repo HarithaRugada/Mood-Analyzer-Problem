9fb69f9cf9f38a147e4cdd28b8d254a85e6a7953
HarithaRugada <haritharugada@gmail.com>
2020-06-20 16:47:19 +0530

    ADDED : Analyze the Mood based on the message given

--- src/main/java/com/moodanalyzer/MoodAnalyzer.java
+++ src/main/java/com/moodanalyzer/MoodAnalyzer.java
@@ -2,5 +2,11 @@ package com.moodanalyzer;
 
 public class MoodAnalyzer
 {
-    
+    public String analyzeMood(String message)
+    {
+        if(message.contains("sad"))
+            return "SAD";
+        else
+            return "HAPPY";
+    }
 }
--- src/test/java/com/moodanalyzer/MoodAnalyzerTest.java
+++ src/test/java/com/moodanalyzer/MoodAnalyzerTest.java
@@ -1,6 +1,23 @@
 package com.moodanalyzer;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MoodAnalyzerTest
 {
+    MoodAnalyzer moodAnalyzer=new MoodAnalyzer();
 
+    @Test
+    public void whenSadMessage_ShouldReturnSad()
+    {
+        String mood=moodAnalyzer.analyzeMood("I am in a sad mood");
+        Assert.assertEquals("SAD",mood);
+    }
+
+    @Test
+    public void whenHappyMessage_ShouldReturnHappy()
+    {
+        String mood = moodAnalyzer.analyzeMood("I am in a happy mood");
+        Assert.assertEquals("HAPPY", mood);
+    }
 }
